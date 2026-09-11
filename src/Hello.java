00251e635fe9221f056264313c7159413739d085
marym <marym.ehab26@yahoo.com>
2026-09-11 11:42:38 -0400

    Add new project

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello world");
+        String hello = "Another hello world";
+        System.out.println(hello);
+    }
+}
---
+++ src/NumberVariables.java
@@ -0,0 +1,8 @@
+public class NumberVariables {
+    public static void main (String [] args){
+        int numOfItems= 10;
+        double itemPrice= 3.5;
+        double totalValue = itemPrice * numOfItems;
+        System.out.println("total value is " + totalValue);
+    }
+}
